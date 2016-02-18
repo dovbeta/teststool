52cc3b6a4e08adaed768b169e9d04d1ec45a8aed
Vasyl Sovyak <v.sovyak@abtollc.com>
2016-02-18 12:59:00 +0200

    Added frontend avatar

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -45,6 +45,7 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <img src="{!! access()->user()->picture !!}" class="user-image" alt="User Image"/>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
